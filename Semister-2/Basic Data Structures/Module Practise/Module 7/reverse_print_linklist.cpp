1b362e94757df2999ef614766bbb5b6607483829
Akash Paul <akashpaul923@gmail.com>
2025-07-05 01:28:07 +0600

    linked list delete and sort practise

---
+++ Semister-2/Basic Data Structures/Module Practise/Module 7/reverse_print_linklist.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node*  &tail, int val)
+{
+    Node* newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_linklist(Node* &head)
+{
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+void reverse_print_linklist(Node* temp)
+{
+    if (temp == NULL)
+    {
+        return;
+    }
+    reverse_print_linklist(temp->next);
+    cout << temp->val << endl;
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    
+    reverse_print_linklist(head);
+
+    return 0;
+}
