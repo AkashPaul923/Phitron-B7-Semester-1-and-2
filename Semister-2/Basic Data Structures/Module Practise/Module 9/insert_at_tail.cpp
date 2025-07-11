f37d0645dfa90fce99332d5ab7f1278416051298
Akash Paul <akashpaul923@gmail.com>
2025-07-11 13:54:28 +0600

    doubly linklist and insertion operation

---
+++ Semister-2/Basic Data Structures/Module Practise/Module 9/insert_at_tail.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node* head)
+{
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void print_backward(Node* tail)
+{
+    Node* temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+void insert_at_head(Node* &head, Node* &tail, int val)
+{
+    Node* newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
+void insert_at_tail(Node* &head, Node* &tail, int val)
+{
+    Node* newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insert_at_tail(head, tail, 200);
+    insert_at_head(head, tail, 100);
+    insert_at_tail(head, tail, 300);
+
+    print_forward(head);
+
+    return 0;
+}
