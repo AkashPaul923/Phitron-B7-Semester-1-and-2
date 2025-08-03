2b9cb62ff234a5be056529881161279ac26893e8
Akash Paul <akashpaul923@gmail.com>
2025-08-04 01:48:11 +0600

    max-heap and min-heap insert operation

---
+++ Semester-2/Basic Data Structures/Module Practise/Module 22/insert_in_max_heap.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(10);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    int val;
+    cin >> val;
+    v.push_back(val);
+    int child = v.size() - 1;
+    while (child != 0)
+    {
+        int parent = (child - 1) / 2;
+        if (v[child] > v[parent])
+            swap(v[child], v[parent]);
+        else
+            break;
+        child = parent;
+    }
+    for (int x : v)
+        cout << x << " ";
+
+    return 0;
+}
