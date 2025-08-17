8c22c60fff8b8ae282132f1e3b99db6767ab6e57
Akash Paul <akashpaul923@gmail.com>
2025-08-17 22:14:48 +0600

    directed and undirected graph represented in adjacency matrix

---
+++ Semester-2/Introduction to Algorithms/Module Practise/Module 1/adjacency_matrix_of_undirected_graph.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int mat[n][n];
+    // set default value 0 instead of using nested loop
+    memset(mat, 0, sizeof(mat));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                mat[i][j] = 1;
+        }
+    }
+
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        mat[a][b] = 1;
+        mat[b][a] = 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
