cd50d29d0f8441ceb5c3e093d8cb2a29cf6d5a95
Akash Paul <akashpaul923@gmail.com>
2025-09-03 18:01:58 +0600

    dsu union and detect cycle usign dsu

---
+++ Semester-2/Introduction to Algorithms/Module Practise/Module 11/detect_cycle_undetected_graph_using_dsu.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+int parent[1005];
+int group_size[1005];
+
+int dsu_find(int node)
+{
+    if (parent[node] == -1)
+    {
+        return node;
+    }
+    int leader = dsu_find(parent[node]);
+    parent[node] = leader;
+    return leader;
+}
+
+void dsu_union(int node1, int node2)
+{
+    int leader1 = dsu_find(node1);
+    int leader2 = dsu_find(node2);
+    if (group_size[leader1] >= group_size[leader2])
+    {
+        parent[leader2] = leader1;
+        group_size[leader1] += group_size[leader2];
+    }
+    else
+    {
+        parent[leader1] = leader2;
+        group_size[leader2] += group_size[leader1];
+    }
+}
+
+int main()
+{
+    memset(parent, -1, sizeof(parent));
+    memset(group_size, 1, sizeof(group_size));
+
+    int n, e;
+    cin >> n >> e;
+    bool isCycle = false;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int leaderA = dsu_find(a);
+        int leaderB = dsu_find(b);
+        if (leaderA == leaderB)
+            isCycle = true;
+        else
+            dsu_union(a, b);
+    }
+
+    cout << (isCycle ? "Cycle Detected" : "No Cycle");
+
+    return 0;
+}
